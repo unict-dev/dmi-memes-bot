0650f3510bda1f5911b3c18ca7f48d1489c0b652
Matilde Morrone <contact@morrone.dev>
2023-12-01 18:27:36 +0100

    Created project skeleton

---
+++ server.ts
@@ -0,0 +1,13 @@
+import { bot } from "./bot.ts";
+import { webhookCallback } from "grammy";
+import { Application, Router } from "oak";
+
+const router = new Router();
+router
+  .post(`/${bot.token}`, webhookCallback(bot, "oak"));
+
+const app = new Application();
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+await app.listen();
---
+++ set_webook.ts
@@ -0,0 +1,3 @@
+import { bot } from "./bot.ts";
+
+bot.api.setWebhook(`https://dmi-memes-bot.unict.dev/${bot.token}`);
